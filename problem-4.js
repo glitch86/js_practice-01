ed263fd91c9ac122fdcd100e5eb3796efce255fb
glitch86 <mdpolokahmed825@gmail.com>
2026-06-08 16:12:26 +0600

    fix(oneliner): added oneliner version of prob-4

--- problem-4.js
+++ problem-4.js
@@ -1,7 +1,10 @@
-const toFahrenheit = (temp) => {
-  let fahrenheit = temp * 1.8 + 32;
-  return fahrenheit;
-};
+// const toFahrenheit = (temp) => {
+//   let fahrenheit = temp * 1.8 + 32;
+//   return fahrenheit;
+// };
+
+// one liner 
+const toFahrenheit = (temp) => temp * 1.8 + 32;
 
 console.log("Temperature in Fahrenheit: ", toFahrenheit(0));
 console.log("Temperature in Fahrenheit: ", toFahrenheit(100));
